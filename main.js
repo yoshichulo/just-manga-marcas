64f2ff61c6a4b7df4bf5c92d11e50a3766b04fb5
Yoshi <joseangelyoshi@gmail.com>
2021-05-31 14:38:42 +0200

    More performance improvements and added JS

---
+++ main.js
@@ -0,0 +1,36 @@
+window.onscroll = function() {scrollFunction()};
+
+// Shows or hides the button depending on the scrolling distance
+function scrollFunction() {
+    var topButton = document.getElementById("topBtn");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    }
+    else {
+        topButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
